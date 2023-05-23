955f8bdcb348c9948e9ed8d572feeda0d2a13bd8
sayf <sayf@multicode.nl>
2023-05-24 00:26:20 +0200

    Adding first unit test

--- src/test/java/org/cavemancode/coupling/AppTest.java
+++ src/test/java/org/cavemancode/coupling/AppTest.java
@@ -1,20 +1,21 @@
 package org.cavemancode.coupling;
 
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
+
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+
+        assertThat(new App()).isNotNull();
     }
 }
